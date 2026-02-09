35db7a7c03df41741f1be7033bbab879f03cf817
devdbsdngml <wh9614y@gmail.com>
2026-02-09 10:20:20 +0900

    feat(phone_input): Define events for phone number input flow

--- lib/app/ui/phone_number_input/phone_number_input_event.dart
+++ lib/app/ui/phone_number_input/phone_number_input_event.dart
@@ -1,4 +1,18 @@
+import 'package:gongbab/domain/entities/lookup/employee_match.dart';
+
 // UI Event
 sealed class PhoneNumberInputEvent {}
 
-class FetchKioskStatus extends PhoneNumberInputEvent {}
+class ScreenInitialized extends PhoneNumberInputEvent {}
+
+class PhoneNumberEntered extends PhoneNumberInputEvent {
+  final String phoneNumber;
+
+  PhoneNumberEntered(this.phoneNumber);
+}
+
+class EmployeeSelected extends PhoneNumberInputEvent {
+  final EmployeeMatch employee;
+
+  EmployeeSelected(this.employee);
+}
